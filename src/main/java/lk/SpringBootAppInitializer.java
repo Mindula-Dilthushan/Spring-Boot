d2fcf03e0544070c148526f8da5c4a4ceada905e
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-11 10:19:40 +0530

    main class create

---
+++ src/main/java/lk/SpringBootAppInitializer.java
@@ -0,0 +1,12 @@
+package lk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppInitializer {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppInitializer.class);
+    }
+}
